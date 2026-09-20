6299d2c1f78be5290baa15f1ce55bf107f0a1b76
Vyshnawee <gvyshnawee@gmail.com>
2026-09-20 09:52:28 +0530

    Fix CORS for production frontend

--- backend/src/main/java/com/tems/config/SecurityConfig.java
+++ backend/src/main/java/com/tems/config/SecurityConfig.java
@@ -50,7 +50,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+        	    "http://localhost:5173",
+        	    "https://team-expense-management-system.vercel.app"
+        	));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
